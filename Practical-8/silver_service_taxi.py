2d9c1994025e255db349052091993e7fa3a9e68d
HtetMyetKo <htetmyetko@my.jcu.edu.au>
2022-01-11 15:23:08 +0630

    Create the other methods

--- Practical-8/silver_service_taxi.py
+++ Practical-8/silver_service_taxi.py
@@ -7,11 +7,20 @@ from taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """Represent a SilverServiceTaxi under Taxi class (which is under Car class)."""
-    flagfall = 4.50
+    flagfall = 4.50  # 2
 
-    def __init__(self, name, fuel, fanciness):
+    def __init__(self, name, fuel, fanciness):  # 1
         """Initialise a SilverServiceTaxi class."""
         super().__init__(name, fuel)  # Given attributes
         self.fanciness = fanciness
         self.price_per_km *= fanciness
 
+
+    def calculate_fare(self):  # 3
+        """Get the current fare."""
+        return self.flagfall + super().calculate_fare()
+
+
+    def __str__(self):  # 4
+        """Return a string representation of a SilverServiceTaxi class."""
+        return "{} plus flagfall of ${:.2f}".format(super().__str__(), self.flagfall)
