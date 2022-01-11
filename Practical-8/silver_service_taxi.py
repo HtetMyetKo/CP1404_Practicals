b404b8888c3778b155f6fbf1cb0fcdc410f39164
HtetMyetKo <htetmyetko@my.jcu.edu.au>
2022-01-11 15:11:27 +0630

    Error correction

--- Practical-8/silver_service_taxi.py
+++ Practical-8/silver_service_taxi.py
@@ -4,3 +4,22 @@ SilverServiceTaxi class
 """
 
 from taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    """Represent a SilverServiceTaxi under Taxi class (which is under Car class)."""
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a SilverServiceTaxi."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
+    def __str__(self):
+        """Return a string representation of a SilverServiceTaxi."""
+        return "{} plus flagfall of ${:.2f}".format(super().__str__(),
+                                                    self.flagfall)
+
+    def get_fare(self):
+        """Get the current fare."""
+        return self.flagfall + super().get_fare()
