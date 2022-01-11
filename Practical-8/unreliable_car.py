e3725e2c18722c31d5185872e7ffb1b50532e40d
HtetMyetKo <htetmyetko@my.jcu.edu.au>
2022-01-11 14:04:18 +0630

    Create cars for testing and make simulation

--- Practical-8/unreliable_car.py
+++ Practical-8/unreliable_car.py
@@ -4,3 +4,20 @@ CP1404/CP5632 UnreliableCar class
 
 import random
 from car import Car
+
+class UnreliableCar(Car):
+    """UnreliableCar class derived from Car"""
+
+    def __init__(self, name, fuel, reliability):
+        """Initialise an UnreliableCar  based on parent class Car."""
+
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive the car with random reliability."""
+        random_number = random.uniform(1, 100)  # Generates a random float
+        if random_number >= self.reliability:
+            distance = 0
+        distance_driven = super().drive(distance) # Signature
+        return distance_driven
