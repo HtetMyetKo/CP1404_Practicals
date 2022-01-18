c1114bd6d196b8fe4261fd4e443016d5b91e8a31
HtetMyetKo <htetmyetko@my.jcu.edu.au>
2022-01-18 18:06:52 +0630

    Get extensions and move files into directories

--- Practical-9/sort_files_1.py
+++ Practical-9/sort_files_1.py
@@ -3,6 +3,30 @@ CP1404/CP5632 Practical
 Sort files Version 1
 """
 import os
-
+import shutil
 
 def main():
+    """Move files into folders with the same name as their extension."""
+    os.chdir("FilesToSort") #Folder
+    for filename in os.listdir('.'):
+        if os.path.isdir(filename):
+            continue
+
+        extension = filename.split('.')[-1]
+        # Get extensions from the respective files
+
+        try:
+            os.mkdir(extension)
+        except FileExistsError:
+            pass
+
+        print("{}/{}".format(extension, filename))
+
+        os.rename(filename, "{}/{}".format(extension, filename))
+
+        shutil.move(filename, extension)
+        # move files into new directories
+
+
+
+main()
