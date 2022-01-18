120ef41b148dc56d9ec303a52b93a1fe546936fa
HtetMyetKo <htetmyetko@my.jcu.edu.au>
2022-01-18 18:19:52 +0630

    Copied the first part from sort_files_1

--- Practical-9/sort_files_2.py
+++ Practical-9/sort_files_2.py
@@ -4,4 +4,13 @@ Sort files Version 1
 """
 import os
 
+def main():
+    """Move files into where user wants to store them based on extension."""
+    # The following dictionary will allow us to map extensions to the destination folder names
+    extension_to_category = {}
+    os.chdir("FilesToSort")
+    for filename in os.listdir('.'):
+        # Ignore directories, just process files
+        if os.path.isdir(filename):
+            continue
 
