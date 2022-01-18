c2968b29beaf73c0b3680d3bc1c1e200d23bd509
HtetMyetKo <htetmyetko@my.jcu.edu.au>
2022-01-18 18:29:16 +0630

    Create dictionary to pair extensions to the directory names

--- Practical-9/sort_files_2.py
+++ Practical-9/sort_files_2.py
@@ -6,11 +6,20 @@ import os
 
 def main():
     """Move files into where user wants to store them based on extension."""
-    # The following dictionary will allow us to map extensions to the destination folder names
+
     extension_to_category = {}
+    # Create dictionary so it will pair extensions to the directory names
+
     os.chdir("FilesToSort")
     for filename in os.listdir('.'):
-        # Ignore directories, just process files
         if os.path.isdir(filename):
             continue
 
+        extension = filename.split('.')[-1]
+        if extension not in extension_to_category:
+            category = input("What category would you like to sort {} files into? ".format(extension))
+            # Get input and pair extensions to the directory names
+            extension_to_category[extension] = category
+
+
+main()
